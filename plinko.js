4d727b7ccd51dee2424f53cd69dfbe7bf442434b
ISHITA-SHAW <83875590+ISHITA-SHAW@users.noreply.github.com>
2021-07-11 16:07:30 +0530

    Update plinko.js

--- plinko.js
+++ plinko.js
@@ -1,12 +1,10 @@
-class plinko{
+class Plinko{
     constructor(x,y){
         var options = {
-            restitution:1,
-            friction:0,
             isStatic:true
         }
         this.r = 10
-        this.bodies = bodies.circle(x,y,this.r,options);
+        this.body = Bodies.circle(x,y,this.r,options);
         World.add(world,this.body)
     }
     display(){
