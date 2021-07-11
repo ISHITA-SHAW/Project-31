49c6f6e6a55c1207cec89c6f22058a8c059b2308
ISHITA-SHAW <83875590+ISHITA-SHAW@users.noreply.github.com>
2021-07-11 15:51:31 +0530

    Update divisions.js

--- divisions.js
+++ divisions.js
@@ -1,10 +1,10 @@
-class division{
+class Division{
     constructor(x,y,w,h){
         var options = {
        
             isStatic:true
         }
-    this.body = bodies.rectangle(x,y,width,height,options);
+    this.body = Bodies.rectangle(x,y,width,height,options);
     this.w = w;
     this.h = h;
     World.add(world,this.body);
@@ -12,9 +12,9 @@ class division{
     }
     display(){
         var pos = this.body.position;
-        rectmode(CENTER);
-        Fill("white");
-        rect(pos.x,pos.y,this.w,this.height);
+        rectMode(CENTER);
+        fill("white");
+        rect(pos.x,pos.y,this.w,this.h);
 
     }
     }
