b27855c81dfb5d1df9521d8e8eb30b899710619e
ISHITA-SHAW <83875590+ISHITA-SHAW@users.noreply.github.com>
2021-07-11 15:52:37 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -1,13 +1,10 @@
 class Ground{
     constructor(x,y,width,height){
       var  options = {
-        isStatic: true,
-        'restitution': 0,
-        'friction':0
-        'density':1
+        isStatic: true
 
     }
-    this.body = bodies.rectangle(x,y,width,height,options);
+    this.body = Bodies.rectangle(x,y,width,height,options);
     this.width = width;
     this.height = height;
 
